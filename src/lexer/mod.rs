839cdf284581b2b80342ed7f96296c83d774eccf
matthewlafrance <128189755+matthewlafrance@users.noreply.github.com>
2024-06-14 14:30:51 -0700

    lexer untested

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,7 +1,7 @@
 mod tokens;
 
 use anyhow::{anyhow, Result};
-use tokens::{Token, Literal, Punctuation};
+use tokens::{Token, Literal, Punctuation, Keyword};
 use std::str::Chars;
 
 pub fn tokenize(src: &str) -> impl Iterator<Item = anyhow::Result<Token>> + '_ {
@@ -29,7 +29,10 @@ impl Iterator for TokenStream<'_> {
     fn next(&mut self) -> Option<Self::Item> {
         self
             .read_int_literal()
-            .or_else(|| self.read_punctuation()).map(Ok)
+            .or_else(|| self.read_punctuation())
+            .or_else(|| self.read_string_literal())
+            .or_else(|| self.read_rest())
+            .map(Ok)
             .or_else(|| self.peek().map(|c| Err(anyhow!("invalid character: {c}"))))
     }
 }
@@ -42,6 +45,7 @@ impl<'a> TokenStream<'a> {
     }
 
     fn read_int_literal(&mut self) -> Option<Token> {
+        self.consume_whitespace();
         let mut int_token = self.peek()?.to_digit(10)?;
         while let Some(int) = self.peek().and_then(|c| c.to_digit(10)) {
             int_token = int_token*10 + int;
@@ -51,7 +55,73 @@ impl<'a> TokenStream<'a> {
     }
 
     fn read_punctuation(&mut self) -> Option<Token> {
+        self.consume_whitespace();
         Punctuation::try_from(self.peek()?).ok().map(Token::Punctuation)
+    }
+
+    fn read_string_literal(&mut self) -> Option<Token> {
+        self.consume_whitespace();
+        match self.peek()? == '"' {
+            true => self.consume_string_literal(),
+            false => None,
+        }
+    }
+
+    fn consume_string_literal(&mut self) -> Option<Token> {
+        let mut string_literal = String::from(self.peek()?);
+        loop {
+            match self.consume_char()? {
+                '"' => {break;},
+                _ => {string_literal.push(self.peek()?);},
+            }
+        }
+        Some(Token::Literal(Literal::Str(string_literal)))
+    }
+
+    fn read_rest(&mut self) -> Option<Token> {
+        self.consume_whitespace();
+        match self.peek()?.is_alphabetic() || self.peek()? == '_' {
+            true => self.consume_rest(),
+            false => None,
+        }
+    }
+
+    fn consume_rest(&mut self) -> Option<Token> {
+        let mut literal = String::from(self.peek()?);
+        loop {
+            match self.consume_char()?.is_alphanumeric() || self.peek()? == '_' {
+                true => {literal.push(self.peek()?);},
+                false => {break;}
+            }
+        }
+        match literal.as_str() {
+            "let" => Some(Token::Keyword(Keyword::Let)),
+            "mut" => Some(Token::Keyword(Keyword::Mut)),
+            "print" => Some(Token::Keyword(Keyword::Print)),
+            "read" => Some(Token::Keyword(Keyword::Read)),
+            "func" => Some(Token::Keyword(Keyword::Func)),
+            "repeat" => Some(Token::Keyword(Keyword::Repeat)),
+            "while" => Some(Token::Keyword(Keyword::Repeat)),
+            "if" => Some(Token::Keyword(Keyword::If)),
+            "else" => Some(Token::Keyword(Keyword::Else)),
+            "true" => Some(Token::Literal(Literal::Bool(true))),
+            "false" => Some(Token::Literal(Literal::Bool(false))),
+            ident => Some(Token::Ident(ident.to_string())),
+        }
+    }
+
+    fn consume_whitespace(&mut self) {
+        loop {
+            match self.peek() {
+                Some(c) => {
+                    match c.is_whitespace() {
+                        true => {self.consume_char();}
+                        false => {break;}
+                    }
+                },
+                None => {break;},
+            }
+        }
     }
 
     fn peek(&self) -> Option<char> {
